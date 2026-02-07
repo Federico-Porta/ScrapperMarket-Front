ba2f6485b11850c1e3c3646fe4e764323eb1c1ff
Federico Porta <federico.porta1197@gmail.com>
2026-02-07 10:32:12 -0300

    Ajustes en app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,24 @@
-import login from './components/login'
+// 1. Corregimos los nombres de las funciones importadas a Mayúsculas
+import Login from './components/login'
 import './bootstrap.min.css'
-import { browserrouter, route, routes } from 'react-router'
-import signup from './components/signup'
-import productlist from './components/productlist'
-import { toastcontainer } from 'react-toastify'
+// 2. Corregimos la librería (es case-sensitive)
+import { BrowserRouter, Route, Routes } from 'react-router-dom' 
+import Signup from './components/signup'
+import ProductList from './components/productlist'
+import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
-import cart from './components/cart'
-import navbar from './components/navbar'
-import userprofile from './components/userprofile'
-import adminpanel from './components/adminpanel'
+import Cart from './components/cart'
+import NavBar from './components/navbar'
+import UserProfile from './components/userprofile'
+import AdminPanel from './components/adminpanel'
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <NavBar />
         <Routes>
+          {/* Ahora las etiquetas coinciden con los imports de arriba */}
           <Route path='/login' element={<Login />} />
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
@@ -24,11 +26,9 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/admin" element={<AdminPanel />} />
-
         </Routes>
-      
       </BrowserRouter>
-       <ToastContainer />
+      <ToastContainer />
     </>
   )
 }
