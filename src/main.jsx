a4f2c7a9c035ae6f390b0c0568056197a9ddfee5
Federico Porta <federico.porta1197@gmail.com>
2026-02-07 10:56:21 -0300

    cambio en app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './bootstrap.min.css'
 import './styles.css'
-import App from './app.jsx'
+// Importamos el archivo y le damos un nombre CUALQUIERA, por ejemplo 'Raiz'
+import Raiz from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Raiz />
   </StrictMode>,
 )
