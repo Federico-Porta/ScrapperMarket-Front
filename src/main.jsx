4c408cd2bc38669a4f36c752a359bb44a7735ce6
Federico Porta <federico.porta1197@gmail.com>
2026-02-07 10:36:29 -0300

    Ajustes en app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './bootstrap.min.css'
 import './styles.css'
-import app from './app.jsx'
+import App from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
