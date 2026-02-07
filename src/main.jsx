c610839742087554110ce5be87a09020207e6d0d
Federico Porta <federico.porta1197@gmail.com>
2026-02-07 11:00:23 -0300

    FIX: Renombrado total para evitar cache de Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './bootstrap.min.css'
 import './styles.css'
 // Importamos el archivo y le damos un nombre CUALQUIERA, por ejemplo 'Raiz'
-import Raiz from './app.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Raiz />
+    <App />
   </StrictMode>,
 )
